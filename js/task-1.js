6696b4b223e25ff9d064de003abd5489a9571aab
SQWOZN <v.n.sinitsa@gmail.com>
2024-03-03 00:58:49 +0200

    task-1,2

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+const categories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${categories.length}`)
+
+categories.forEach(element => {
+    const categoryName = element.querySelector("h2").textContent;
+    const categoryLength = element.querySelectorAll("li").length;
+    console.log(`Category: ${categoryName}`);
+    console.log(`Elements: ${categoryLength}`);
+})
+
+
+
